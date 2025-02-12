849be24edc8f3bf014694f104520adb51887be85
DevLBjapan <nakajima@landbridge.co.jp>
2025-02-12 21:12:39 +0900

    Update code

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 import express from "express";
 import dotenv from "dotenv";
 
-// 他のルート例
+// 他のルート
 import webhookRouter from "./routes/webhook.js"; // デフォルトインポート
 import mailRouter from "./routes/mail.js";
 import reportRouter from "./routes/report.js";
@@ -13,6 +13,12 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+// ★ ルートパス ("/") を追加
+// ALBのヘルスチェックなどが http://<ALB DNS>/ にアクセスした際、200を返す
+app.get("/", (req, res) => {
+  res.status(200).send("OK from root path");
+});
 
 // /webhook
 app.use("/webhook", webhookRouter);
